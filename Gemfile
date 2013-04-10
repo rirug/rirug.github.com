11bfe0df1f6064019025f8b73a64ec6577f26929
Joe Alba <joe@joealba.com>
2013-04-09 23:39:47 -0400

    Tried to add haml support.  I do not think Github pages will like it though.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ gem 'jekyll'
 gem 'rake'
 gem 'maruku'
 gem 'sass'
+gem "haml", "~> 4.0.2"
 gem "compass", "~> 0.12.2"
 gem "zurb-foundation", "~> 4.0.9"
-# gem "rb-fsevent", "~> 0.9.3"
+gem "rb-fsevent", "~> 0.9.3"
 gem "rack-jekyll", "~> 0.4.0"
---
+++ _plugins/haml.rb
@@ -0,0 +1,23 @@
+require 'haml'
+
+module Jekyll
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /haml/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      engine = Haml::Engine.new(content)
+      engine.render
+    rescue StandardError => e
+      puts "!!! HAML Error: " + e.message
+    end
+  end
+end
